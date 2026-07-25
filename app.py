8a3e7756fabf4e118aa6b564f0b27d9f21eaa23c
noventra <ninokvlividze20@gmail.com>
2026-07-25 12:53:19 +0400

    Add permanent user region on registration, live leaderboard and sponsor bar

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ class User(db.Model, UserMixin):
     is_admin = db.Column(db.Boolean, default=False)
     phone = db.Column(db.String(20), default="")
     bank_account = db.Column(db.String(50), default="")
+    region = db.Column(db.String(50), default="tbilisi")  # <--- აი ეს ხაზი დაემატა
     transactions = db.relationship('Transaction', backref='user', lazy=True)
     withdrawals = db.relationship('WithdrawalRequest', backref='user', lazy=True)
     last_seen_board = db.Column(db.DateTime, default=db.func.current_timestamp())
